6c021fc891ee0ee02537d038937c44d6aff8f65a
Michael Yakovlev <kvadrofilii@gmail.com>
2022-02-13 15:03:59 +0300

    refactor: hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -6,16 +6,18 @@ const hamburgerList = pageHeader.querySelector('.hamburger__list');
 const hamburgerBtn = pageHeader.querySelector('.hamburger__btn');
 
 hamburgerBtn.addEventListener('click', () => {
-	hamburgerList.classList.toggle("hamburger__toggle");
-	pageHeader.classList.toggle("hamburger__toggle");
-	body.classList.toggle("hamburger__toggle");
+	toggleClass();
 })
 
 hamburgerList.addEventListener('click', (event) => {
 	const target = event.target;
 
 	if (target.tagName !== 'A') return;
+	toggleClass();
+})
+
+function toggleClass() {
 	hamburgerList.classList.toggle("hamburger__toggle");
 	pageHeader.classList.toggle("hamburger__toggle");
 	body.classList.toggle("hamburger__toggle");
-})
+}
